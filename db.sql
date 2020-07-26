9dc864fec2156605094c890defbe0b65866dcb56
jhs512 <jangka512@gmail.com>
2020-07-26 13:48:42 +0900

    게시물 작성시 로그인 된 회원의 번호가 article 테이블에 같이 저장되도록 작업

--- db.sql
+++ db.sql
@@ -37,3 +37,31 @@ CREATE TABLE `member` (
     `name` CHAR(100) NOT NULL
 );
 
+# 회원 테이블 데이터 생성
+INSERT INTO `member` 
+SET regDate = NOW(),
+loginId = 'admin',
+`loginPw` = 'admin',
+`name` = '관리자';
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+loginId = 'user1',
+`loginPw` = 'user1',
+`name` = '유저1';
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+loginId = 'user2',
+`loginPw` = 'user2',
+`name` = '유저2';
+
+# 게시물 테이블에 memberId 칼럼 추가
+ALTER TABLE `article` ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER regDate;
+
+# 기존 게시물은 그냥 2번 회원이 전부 작성한걸로 정한다.
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
+
+
